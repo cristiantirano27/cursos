85ec2695951263fd2c8adf604c20ce355f4eceb1
cristiantirano27 <catirano5@misena.edu.co>
2021-01-14 12:40:15 -0500

    chore: Tipo de datos + comentarios, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/tipoDatos.py
@@ -0,0 +1,31 @@
+# Variable: Palabra que puede almacenar algun tipo de dato
+''' Tipos: 
+numero 
+coma flotante
+texto
+booleano
+'''
+# Se declara un tipo de variable(numero), con su valor, y se imprime.
+numero = 10
+print(numero)
+print(type(numero))
+print("--------------------")
+# Se declara un tipo de variable(flotante), con su valor, y se imprime.
+numeroComaFlotante = 10.25
+print(numeroComaFlotante)
+print(type(numeroComaFlotante))
+print("--------------------")
+texto = "hola"
+print(texto)
+print(type(texto))
+print("--------------------")
+booleano = True
+print(booleano)
+print(type(booleano))
+print("--------------------")
+suma = numero + numeroComaFlotante
+print(suma)
+print(type(suma))
+suma = texto
+print(suma)
+print(type(suma))
