091dcda1100afb9adf165ae60a7bac654de3cc8a
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 15:25:43 -0500

    chore: Ejercicio 05, Intercambio de valores de dos variables, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/ejercicios/ejercicio05.py
@@ -0,0 +1,9 @@
+print("Ingrese los valores a solicitar: ")
+a = int(input("El valor de a: "))
+b = int(input("El valor de b: "))
+aux = a
+a = b
+b = aux
+print("Los valores de a y b son: ")
+print(f"Valor de a: {a}")
+print(f"Valor de b: {b}")
