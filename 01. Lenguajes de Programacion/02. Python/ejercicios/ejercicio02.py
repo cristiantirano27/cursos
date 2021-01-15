42d299fed1bd6842e48698bbcc6de99a56286be7
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 14:28:03 -0500

    chore: Ejercicio 02, operadores aritmeticos, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/ejercicios/ejercicio02.py
@@ -0,0 +1,18 @@
+'''
+Declarar multiples variables.
+Pedir al usuario que ingrese el valor de 2 variables 
+Realizar varias operaciones aritmeticas (Suma, resta, multiplicacion, division y division entera)
+Mostrar el resultado con print.
+'''
+n1 = float(input("Ingrese el numero 1: "))
+n2 = float(input("Ingrese el numero 2: "))
+s = n1 + n2
+r = n1 - n2
+m = n1 * n2
+d = n1 / n2
+de = n1 // n2
+print(f"Suma = {s}")
+print(f"Resta = {r}")
+print(f"Multiplicacion = {m}")
+print(f"Division = {d}")
+print(f"Division entera = {de}")
