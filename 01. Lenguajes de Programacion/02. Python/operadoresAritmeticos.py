6c8656d9e8f6bb076516cc611f413a860600b6fc
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 14:11:18 -0500

    chore: Operadores aritneticos, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/operadoresAritmeticos.py
@@ -0,0 +1,16 @@
+num1 = 5
+num2 = 3
+resultado = num1 + num2
+print("El resultado de la suma es: " ,resultado)
+resultado = num1 - num2
+print("El resultado de la resta es: ",resultado)
+resultado = num1 * num2
+print("El resultado de la multiplicacion es: ",resultado)
+resultado = num1 / num2
+print("El resultado de la division es: ",resultado)
+resultado = num1 // num2
+print("El resultado de la division entera es: ",resultado)
+resultado = num1 % num2
+print("El resultado del modulo es: ",resultado)
+resultado = num1 ** num2
+print("El resultado del exponente es: ",resultado)
