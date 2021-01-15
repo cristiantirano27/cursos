7c32024d68f5db350990ff759da3f8fd471bb4bc
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 14:14:55 -0500

    chore: Operadores relacionales, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/operadoresRelacionales.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 20
+num3 = 30
+resultado = num1 < num2
+print(num1," es menor que ",num2," : ",resultado)
+resultado = num1 > num2
+print(num1," es mayor que ",num2," : ",resultado)
+resultado = num1 == num2
+print(num1," es igual que ",num2," : ",resultado)
+resultado = num1 <= num2
+print(num1," es menor igual que ",num2," : ",resultado)
+resultado = num1 >= num2
+print(num1," es mayor igual que ",num2," : ",resultado)
+resultado = num1 != num2
+print(num1," es diferente que ",num2," : ",resultado)
+resultado = num1 + num2 == num3
+print(num1 + num2," es igual que ",num3," : ",resultado)
