c8e0acc2aa57ead953cde8fee80bc8ab610e6dcf
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 14:22:18 -0500

    chore: Usando la funcion print, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/salidaConsola.py
@@ -0,0 +1,5 @@
+nombre = "Cristian"
+edad = 20
+# print("Hola, tu nombre es ",nombre," y tu edad es ",edad," años")
+# print("Hola, tu nombre es {0} y tu edad es {1:10.3f} años. Adios {0}".format(nombre,edad))
+print(f"Hola, tu nombre es {nombre} y tu edad es {edad:10.3f} años. Adios {nombre}")
