34abff5c51fc152831660fb37a1f260a28938806
cristiantirano27 <catirano5@misena.edu.co>
2021-01-15 14:24:43 -0500

    chore: Como usar la funcion input, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/entradaDatos.py
@@ -0,0 +1,6 @@
+nombre = input("Ingresa tu nombre: \n")
+edad = input("Ingresa tu edad: \n")
+edad = int(edad)
+print(f"Tu nombre es: {nombre}, y tu edad es de {edad} años")
+dias = edad * 365
+print(f"Naciste hace {dias:,} dias")
