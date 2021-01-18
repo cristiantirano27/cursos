aa6e708f8fd1db8338d582488a7b2e27e66fe57a
cristiantirano27 <catirano5@misena.edu.co>
2021-01-18 13:27:37 -0500

    Update: Problema 03, IGV y Precio Total, en Java

--- 01. Lenguajes de Programacion/01. Java/Java/src/lfp/cap1/estructurasecuencial/problemas/Problema03.java
+++ 01. Lenguajes de Programacion/01. Java/Java/src/lfp/cap1/estructurasecuencial/problemas/Problema03.java
@@ -1,14 +1,27 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 3. Dado el valor de venta de un producto, hallar el IGV (19%) y el precio de venta.
  */
 package lfp.cap1.estructurasecuencial.problemas;
+
+import java.util.Scanner;
 
 /**
  *
  * @author cristian tirano
  */
 public class Problema03 {
-    
+    public static void main (String [] args){
+        float vv, igv, pv;
+		
+	Scanner teclado = new Scanner(System.in);
+	System.out.print("Valor de venta: \n");
+	vv = teclado.nextFloat();
+	
+	igv = vv * 0.19F;
+	pv = vv + igv;
+		
+	System.out.println("");
+	System.out.println("IGV: " + igv);
+	System.out.println("Precio de venta: " + pv);
+    }
 }
