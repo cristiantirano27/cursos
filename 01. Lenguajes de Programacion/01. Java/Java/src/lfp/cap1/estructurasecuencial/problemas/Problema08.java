032624ace6c808b3f36e7c778965d7aef80deedf
cristiantirano27 <catirano5@misena.edu.co>
2021-02-01 13:05:57 -0500

    update: Problema 08, Calcular interes compuesto, en Java

--- 01. Lenguajes de Programacion/01. Java/Java/src/lfp/cap1/estructurasecuencial/problemas/Problema08.java
+++ 01. Lenguajes de Programacion/01. Java/Java/src/lfp/cap1/estructurasecuencial/problemas/Problema08.java
@@ -1,7 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 8. Calcular el interes compuesto generado por un capital depositado durante cierta cantidad de tiempo a una tasa de interes 
+ * determinada, aplique las siguientes formulas:
+ * M = (1 + r%)*.C
+ * I = M - C
+ * Monto (M): Es la suma del capital mas sus intereses producido en determinado tiempo.
+ * Tasa de Interes (r%): Es la ganancia que se obtiene por cada 100 unidades monetarias en cada periodo de tiempo.
+ * Capital (C): Es todo aquello que se va a ceder o imponer durante algun tiempo para generar una ganancia.
+ * Interes (I): Parte de la utilidad que obtiene el capital  prestar su dinero.
+ * Tiempo (t): Es el periodo de tiempo durante el cual se cede el capital. 
  */
 package lfp.cap1.estructurasecuencial.problemas;
 
@@ -13,15 +19,21 @@ import java.util.Scanner;
  */
 public class Problema08 {
     public static void main(String[] args) {
-	int n, s;
+	float c, r, t, i, m;
 		
 	Scanner teclado = new Scanner(System.in);
-	System.out.print("Numero: ");
-	n = teclado.nextInt();
+	System.out.print("Capital: \n");
+	c = teclado.nextFloat();	
+	System.out.print("Tasa de interes: \n");
+	r = teclado.nextFloat();
+	System.out.print("Tiempo: \n");
+	t = teclado.nextFloat();
 		
-	s = (n *(n + 1)) / 2;
+	m = (float)(Math.pow((double) (1 + r / 100), (double)t)) * c;
+	i = m - c;
 		
 	System.out.println("");
-	System.out.println("Suma: " + s);
+	System.out.println("Interes: " + i);
+	System.out.println("Monto: " + m);
     }
 }
