24a15e78767dae3218508a124938cca0cb15e262
cristiantirano27 <catirano5@misena.edu.co>
2021-02-10 10:39:06 -0500

    chore: Update ejercicio 10

--- 01. Lenguajes de Programacion/02. Python/ejercicios/ejercicio10.py
+++ 01. Lenguajes de Programacion/02. Python/ejercicios/ejercicio10.py
@@ -3,6 +3,6 @@ l = l.lower()
 if l == "a" or l == "e" or l == "i" or l == "o" or l == "u":  
         print("Error!!. Pedi una letra tipo consonante")
 if l == "á" or l == "é" or l == "í" or l == "ó" or l == "ú":
-        print(f"La letra {l} lleva tilde, por lo tanto no es una consonante")
+        print("La letra ingresada lleva tilde, por lo tanto no es una consonante")
 else:
     print("La letra ingresada es consonante")
