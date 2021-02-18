620b7c0fa584b73a76ccd28c3a4a333561cb829c
cristiantirano27 <catirano5@misena.edu.co>
2021-02-18 11:56:15 -0500

    chore: Conjuntos, en Python

---
+++ 01. Lenguajes de Programacion/02. Python/conjuntos.py
@@ -0,0 +1,8 @@
+conjuntos = set()
+conjuntos = {1, 2, 3, "Hola", "usuario"}
+print(conjuntos)
+conjuntos.add(5)
+print(conjuntos)
+print("3 in conjuntos = ", 3 in conjuntos)
+conjuntos.discard(1)
+print("conjuntos.discard(1) = ", conjuntos)
