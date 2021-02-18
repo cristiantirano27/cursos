7f2ac8c4fa7d4a188a5a36a0ef41cc2cf589ceba
cristiantirano27 <catirano5@misena.edu.co>
2021-02-18 10:39:02 -0500

    chore: Problema 15, Par o impar, en Java

---
+++ 01. Lenguajes de Programacion/01. Java/Java/src/lfp/cap2/estructuraselectivasimpleydoble/problemas/Problema15.java
@@ -0,0 +1,32 @@
+/*
+ * 15. Determinar si un numero entero es par o impar.
+ */
+package lfp.cap2.estructuraselectivasimpleydoble.problemas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cristian tirano
+ */
+public class Problema15 {
+    public static void main(String[] args) {
+        int n;
+        String r = "";
+    
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Numero: ");
+        n = teclado.nextInt();
+        
+        if(n % 2 == 0){
+            r = "PAR";
+        }else{
+            r = "IMPAR";
+        }
+        
+        System.out.println("");
+        System.out.println("Resultado: " + r);
+    }
+    
+}
+
